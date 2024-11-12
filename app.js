5632ef4e8ac7e2aad2ce2a2d623aa6e656628b16
Logan <logan.gil@uitsolutions.com>
2024-11-12 14:02:55 +0900

    fix:logging system

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 require("dotenv").config();
+const logger = require("./winston");
 
 const router = require("./route");
 
 const createApp = () => {
     const app = express();
     app.use(cors());
-    app.use(morgan('dev'));
+    app.use(morgan(":method :status :url :response-time ms", { stream: logger.stream }));
     app.use(express.json());
     app.use(router);
 
